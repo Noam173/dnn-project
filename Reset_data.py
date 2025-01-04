2b72689a59d267f66c0af934db1cc010f5e8ce29
Noamiko173 <Noamiko173@gmail.com>
2025-01-04 14:04:51 +0200

    Update Reset_data.py

--- Reset_data.py
+++ Reset_data.py
@@ -5,9 +5,8 @@ Spyder Editor
 This is a temporary script file.
 """
 import shutil
-import os
-import cudf as pd
 import Data_Manipolation
+
 data=Data_Manipolation.create_directory()
 shutil.rmtree(f'{data}')
 
