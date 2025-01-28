f5a408c08b26e5db715380c7c187bee292712f18
Noamhotcoffee11 <117226467+Noamhotcoffee11@users.noreply.github.com>
2025-01-29 00:35:16 +0200

    Update encode_data.py

--- encode_data.py
+++ encode_data.py
@@ -28,7 +28,7 @@ def encoder(path):
     
     path=os.path.expanduser(path)
     
-    data=pd.read_csv(path, dtype={'label': int})
+    data=pd.read_csv(path)
     
     data = data[data['label'] != -1]
             
