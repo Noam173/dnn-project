d712c0a47dbb2376188d9045f6072eda9917cccb
Noam <Noamiko173@gmail.com>
2025-02-02 12:58:34 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -5,12 +5,12 @@ import seaborn as sns
 def plot_training(history):
 
     data=pd.DataFrame.from_dict(history)
-    data.index=data.index+1
+    data['epochs']=data.index+1
 
-    sns.lineplot(x=data.index, y=data.accuracy, label='Train accuracy')
-    sns.lineplot(x=data.index, y=data.val_accuracy, label='Val accuracy')
+    sns.lineplot(x=data.epochs, y=data.accuracy, label='Train accuracy')
+    sns.lineplot(x=data.epochs, y=data.val_accuracy, label='Val accuracy')
     plt.show()
     
-    sns.lineplot(x=data.index, y=data.loss, label='Train loss')
-    sns.lineplot(x=data.index, y=data.val_loss, label='Val loss')
+    sns.lineplot(x=data.epochs, y=data.loss, label='Train loss')
+    sns.lineplot(x=data.epochs, y=data.val_loss, label='Val loss')
     plt.show()
